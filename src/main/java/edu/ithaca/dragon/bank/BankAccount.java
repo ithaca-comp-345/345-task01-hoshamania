085b675f02890ee2e1119c6bacceb7145061a61b
NusiratO <nolumegbon@ithaca.edu>
2021-01-27 22:59:40 -0500

    It's not completely done, but it's what I have

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -43,8 +43,43 @@ public class BankAccount {
         if (email.indexOf('@') == -1){
             return false;
         }
-        else {
+        else if(email.indexOf('.') == -1){
+            return false;
+        }
+
+        String[] invalidChar = {"!","#","$","&"};
+        int count = 0;
+        for (int i = 0; i < invalidChar.length; i++){
+            String symbol = invalidChar[i];
+            count += email.chars().filter(ch -> ch == symbol.charAt(0)).count();count += email.chars().filter(ch -> ch == symbol.charAt(0)).count();
+            if (count > 0){
+                return false;
+            }
+        }
+
+        String[] session = email.split("@", 2);
+        
+        if(session[0].indexOf(".") == 0){
+            return false;
+        }
+        else if(session[0].indexOf("-") == 0){
+            return false;
+        }
+        else if(session[0].indexOf("_") == 0){
+            return false;
+        }
+        else if (session[0].indexOf('.') == session[0].length()-1){
+            return false;
+        }
+        else if (session[0].indexOf('-') == session[0].length()-1){
+            return false;
+        }
+        else if (session[0].indexOf('_') == session[0].length()-1){
+            return false;
+        }
+        else{
             return true;
         }
+
     }
 }
